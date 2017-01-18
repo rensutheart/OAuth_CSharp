f324ffa952f1bd3ecb9b4d0c9c189d36b120bf35
rensutheart <rensutheart@users.noreply.github.com>
2017-01-18 10:53:22 +0200

    Updated OAuth_CSharp to allow custom parameters

--- OAuth_CSharp.cs
+++ OAuth_CSharp.cs
@@ -8,7 +8,6 @@ using System.Text;
 
 public class OAuth_CSharp
 {
-
     private string oauthConsumerKey = null;
     private string oauthConsumerSecret = null;
     private string oauthToken = null;
@@ -44,6 +43,17 @@ public class OAuth_CSharp
         GenerateDictionary();
     }
 
+    public string GenerateRequestURL(string url, string HTTP_Method , List<string> parameters)
+    {
+        outputString = url;
+
+        // outputString is changed in GenerateSignatureBase and GenerateSignature
+        string signatureBase = GenerateSignatureBase(url, HTTP_Method, parameters);
+        string signature = GenerateSignature(signatureBase);
+        
+        return outputString;
+    }
+
     public string GenerateRequestURL(string url, string HTTP_Method)
     {
         outputString = url;
@@ -51,7 +61,7 @@ public class OAuth_CSharp
         // outputString is changed in GenerateSignatureBase and GenerateSignature
         string signatureBase = GenerateSignatureBase(url, HTTP_Method);
         string signature = GenerateSignature(signatureBase);
-        
+
         return outputString;
     }
 
@@ -114,14 +124,48 @@ public class OAuth_CSharp
         };
     }
 
-    private string GenerateSignatureBase(string url, string HTTP_Method)
+    private string GenerateSignatureBase(string url, string HTTP_Method, List<string> parameters)
     {
+        if(parameters == null)
+        {
+            Debug.Log("Invalid parameters supplied: null. Running without parameters.");
+            return GenerateSignatureBase(url, HTTP_Method);            
+        }
+        if (parameters.Count != 0)
+        {
+            foreach (string parameter in parameters)
+            {
+                //Debug.Log(parameters);
+                string[] elements = parameter.Split(new char[] { '=' });
+                if (elements.Length % 2 != 0)
+                {
+                    Debug.LogWarning("Invalid parameter supplied");
+                }
+                else
+                {
+                    try
+                    {
+                        urlParamsDictionary.Add(elements[0], elements[1]);
+                    }
+                    catch(ArgumentException argExc)
+                    {
+                        Debug.LogWarning("Parameter key already in dictionary: " + elements[0]);
+                    }
+                }
+            }
+        }
+
+        return GenerateSignatureBase(url, HTTP_Method);
+    }
+    private string GenerateSignatureBase(string url, string HTTP_Method)
+    { 
         string parameterString = String.Empty;
+        
         int totalKeys = urlParamsDictionary.Keys.Count;
         int keyCount = 0;
         foreach (KeyValuePair<string, string> keyValuePair in urlParamsDictionary)
         {
-            //Debug.Log("Appended to output: " + keyValuePair.Key + "=" + keyValuePair.Value);
+           // Debug.Log("Appended to output: " + keyValuePair.Key + "=" + keyValuePair.Value);
             parameterString += keyValuePair.Key + "=" + keyValuePair.Value;
 
             if (++keyCount != totalKeys) // indicates the last item is found
@@ -129,11 +173,11 @@ public class OAuth_CSharp
         }
         //parameterString = "oauth_consumer_key=ck_eb425c3f55f09555c61b5c49c19c40fe170cc761&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1484230019&oauth_nonce=wI6kZx";
         outputString += "?" + parameterString;
-        Debug.Log(parameterString);
+        //Debug.Log(parameterString);
 
         string signatureBase = HTTP_Method.ToUpper() + "&" + Uri.EscapeDataString(url) + "&" + Uri.EscapeDataString(parameterString);
         
-        Debug.Log(signatureBase);
+        Debug.Log("Signature Base: " + signatureBase); ;
         return signatureBase;
     }
 
